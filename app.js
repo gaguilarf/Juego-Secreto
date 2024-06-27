c654a01c8b4a77f55753148f40256af579fbd384
Gustavo Fernando Aguilar Flores <34445755+gaguilarf@users.noreply.github.com>
2024-06-26 20:50:26 -0500

    Version final Juego numero secreto

---
+++ app.js
@@ -0,0 +1,60 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = document.querySelector('#valorUsuario').value;
+    if(numeroDeUsuario == numeroSecreto){
+        asignarTextoElemento('p', `¡Adivinaste! en ${intentos} ${(intentos==1) ? 'intento' : 'intentos'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if(numeroDeUsuario>numeroSecreto){
+            asignarTextoElemento('p', '¡El numero secreto es menor!');
+        } else {
+            asignarTextoElemento('p', '¡El numero secreto es mayor!');
+        }
+        intentos++;
+        limpiarCampo();
+    }
+}
+
+function condicionesIniciales(){
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    asignarTextoElemento('p', `Indica un numero entre 1 y ${numeroMaximo}`);
+    asignarTextoElemento('h1', 'Juego del numero secreto');
+}
+
+function limpiarCampo(){
+    document.querySelector('#valorUsuario').value = '';
+
+}
+
+function reiniciarJuego(){
+    limpiarCampo();
+    condicionesIniciales();
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
+}
+
+function generarNumeroSecreto(){
+    let numeroGenerado = Math.floor(Math.random() * 10) + 1;
+    if(listaNumerosSorteados.length == numeroMaximo){
+        return asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles');
+    } else {
+        if(listaNumerosSorteados.includes(numeroSecreto)){
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+    
+}
+
+condicionesIniciales();
